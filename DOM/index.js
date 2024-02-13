f27bf3ec2b75fc44ab9059288a688c89b5f251f1
Sachin Gavali <sachingavali321@gmail.com>
2024-02-13 21:53:12 +0530

    dom

---
+++ DOM/index.js
@@ -0,0 +1,8 @@
+const h1 = document.querySelector("h1");
+console.log(h1);
+const span = document.querySelector("span");
+const h2 = document.getElementById("heading");
+const h3 = document.getElementsByName("heading");
+console.log(h2);
+console.log(h3);
+function ch() {}
