f76b526b2557d4060b09247bfb2ab5510aab1dd1
Sachin Gavali <sachingavali321@gmail.com>
2024-03-20 09:43:34 +0530

    dom

--- DOM/index.js
+++ DOM/index.js
@@ -1,12 +1,6 @@
 const h1 = document.querySelector("h1");
-
-const span = document.querySelector("span");
-const h2 = document.getElementById("heading");
-const h3 = document.getElementsByName("heading");
-const input = document.querySelector("input");
-function ch() {
-  h1.innerText = "Hey";
-  input.setAttribute("placeholder", "New Placeholder");
-  console.log(input.getAttribute("name"));
-  input.setAttribute("class", "input__box");
-}
+const main = document.querySelector("main");
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", function () {
+  main.style.backgroundColor = "red";
+});
